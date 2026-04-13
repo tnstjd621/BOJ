5bcc8b0fbb6ed2973a7eb4bd08bf842540c51445
tnstjd <0621danny@naver.com>
2026-04-13 17:03:01 +0900

    P1699, P17626

---
+++ Silver/P17626.java
@@ -0,0 +1,22 @@
+// P17626 Four Squares Silver III
+package Silver;
+import java.util.*;
+public class P17626 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] dp = new int[num+1];
+        dp[1] = 1;
+        int min = 0;
+        for(int i = 2; i <= num; i++){
+            min = Integer.MAX_VALUE;
+            for(int j = 1; j*j <= i; j++){
+                int temp = i - j*j;
+                min = Math.min(min, dp[temp]);
+            }
+            dp[i] = min+1;
+        }
+        System.out.print(dp[num]);
+    }
+}
